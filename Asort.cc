a4f0e11a59c0145b8ac6a1297c68b479d2313d11
cmcanult11 <cmcanult@villanova.edu>
2025-11-11 15:42:10 -0500

    programmed A sort function (our inserstion method)

--- Asort.cc
+++ Asort.cc
@@ -7,8 +7,18 @@
 
 #include "myheaders.h"
 
-
 // Your ASort function(s) goes here ..
+template<classT>
+void inerstionSort(T data[], int n){
+	for(int i = 1, j; i < n; i++){
+		T tmp = data[i];
+		for(j = i; j > 0 && tmp < data[j-1]; j--){
+			data[j] = data[j-1];
+		}
+		data[j] = tmp;
+	}
+}
+
 
 
 
