a4dea25f58c8da275499ea0b0d55b01a7353dd2a
karalindonen <soccerlefty25@gmail.com>
2025-11-17 21:01:20 -0500

    started writing csort

---
+++ Csort.cc
@@ -0,0 +1,16 @@
+#include "myheaders.h"
+
+//Csort function
+void mergeSort(long data[], long first, long last){
+	//create temp variable
+	long mid;
+	if (first < last){
+		mid = (first + last)/2;
+		//mergeSort(data, first, mid);
+		//mergeSort(data, mid+1, last);
+		//merge(data, first, last);
+	}
+}
+
+
+
