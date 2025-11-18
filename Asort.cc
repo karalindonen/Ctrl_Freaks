05bd27c3f053224eecf82d67329ab753d77b22ae
mayaharner <mayaharner9@gmail.com>
2025-11-18 14:45:07 -0500

    update

--- Asort.cc
+++ Asort.cc
@@ -1,19 +1,5 @@
 
 #include "myheaders.h"
 
-//Asort function
-void insertionSort(long data[], long n){
-	for(long i = 1, j; i < n; i++){
-		long tmp = data[i];
-		for(j = i; j > 0 && tmp < data[j-1]; j--){
-			data[j] = data[j-1];
-		}
-		data[j] = tmp;
-	}
-}
-
-
-
-
 
 
