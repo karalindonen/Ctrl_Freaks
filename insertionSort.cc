323d2ea5d0a40e5537eb569257a0eb5c41de6047
mayaharner <mayaharner9@gmail.com>
2025-11-18 15:16:22 -0500

    updated comments

--- insertionSort.cc
+++ insertionSort.cc
@@ -1,18 +1,18 @@
 
 #include "myheaders.h"
 
-//Asort function
+//Asort function - Insertion Sort
 void insertionSort(long data[], long n){
 	for(long i = 1, j; i < n; i++){
 		// create temp variable to copy the element
 		long tmp = data[i];
 
 		for(j = i; j > 0 && tmp < data[j-1]; j--){
-			// if the next element is greater then the previous element
-			// then shift the previous element by one position
+			// starting with second element, if the temp variable is less than the previous
+			// element, then shift the previous element by one position
 			data[j] = data[j-1];
 		}
-		//update the second element
+		// place data[i] in proper position
 		data[j] = tmp;
 	}
 }
