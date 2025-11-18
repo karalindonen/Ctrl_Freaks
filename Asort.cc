61ee6ae9f087fdf237766e333d2c4167a6621de1
mayaharner <mayaharner9@gmail.com>
2025-11-18 14:57:20 -0500

    updated with comments

--- Asort.cc
+++ Asort.cc
@@ -1,5 +1,19 @@
 
 #include "myheaders.h"
 
+//Asort function
+void insertionSort(long data[], long n){
+	for(long i = 1, j; i < n; i++){
+		// create temp variable to copy the element
+		long tmp = data[i];
 
+		for(j = i; j > 0 && tmp < data[j-1]; j--){
+			// if the next element is greater then the previous element
+			// then shift the previous element by one position
+			data[j] = data[j-1];
+		}
+		//update the second element
+		data[j] = tmp;
+	}
+}
 
