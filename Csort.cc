fe56a4c54eac2ec2b6f46e7a38eba95eae250398
cmcanult11 <cmcanult@villanova.edu>
2025-11-18 14:24:31 -0500

    coded C sort - bubble sort

--- Csort.cc
+++ Csort.cc
@@ -1,4 +1,11 @@
 #include "myheaders.h"
-
-
+void bubbleSort(long data[], long n){
+	for(long i = 0; i < n-1;i++){
+		for(long j = n-1; j > i; --j){
+			if( data[j] < data[j-1]){
+				swap(data[j], data[j-1]);
+			}
+		}
+	}
+}
 
